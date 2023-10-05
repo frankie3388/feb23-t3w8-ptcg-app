8e368fd56f9f44a6761c2e36e17bec2b23bae2f7
“frankie3388” <frankiejuen33@gmail.com>
2023-10-05 23:05:54 +1000

    fetch request working and env working

--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -5,7 +5,7 @@ export const ApiContext = createContext(null)
 
 export default function ApiProvider({children}){
 
-	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/");
+	const [apiUrl, setApiUrl] = useState("https://api.pokemontcg.io/v2/");
 
 	// useEffect(() => {
 	// 	setApiUrl("https://api.pokemontcg.io/");
@@ -14,7 +14,7 @@ export default function ApiProvider({children}){
 	return (
         <ApiContext.Provider value={
 			{
-				apiUrl: apiUrl, 
+				api: apiUrl, 
 				setApi: setApiUrl
 			}
 		}>
